3492d234b86b8319f630bab649a628d836726c43
swappel <sebastian.lauber38@gmail.com>
2026-04-22 19:56:54 +0200

    Added TODOs to main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main() {
-
     // TODO: Implement general loop for app
-
+    // TODO: Implement UI
+    // TODO: Implement pre-save cache for changes
+    // TODO: Implement change review
+    // TODO: Implement instant search in ui
 }
