3aa229215a98959828176b54e4375cae6d1c74df
swappel <sebastian.lauber38@gmail.com>
2026-05-11 11:39:22 +0200

    Made general start to program, implementing `int main()` function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,61 @@
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <GLFW/glfw3.h>
+
 #include "ui/MainUI.h"
 
 int main()
 {
-    // TODO: Implement general loop for app
-    // TODO: Implement UI
-    // TODO: Implement pre-save cache for changes
-    // TODO: Implement change review
-    // TODO: Implement instant search in ui
+    // Setup GLFW and Window
+    if (!glfwInit()) return 1;
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Localization Editor", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
+    // Setup Dear ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    // Initialize UI Controller
+    MainUI myUI;
+    // TODO: Load files here?
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Start the ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        myUI.Render();
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
